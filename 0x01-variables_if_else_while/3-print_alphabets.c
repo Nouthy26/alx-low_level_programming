0691e9b72e19852be8d807a1b439da35f2b858bc
Nouthy26 <nkosinothando67@gmail.com>
2023-07-14 12:04:49 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,8 +7,10 @@ int main(void)
 {
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
-for (i = 0 < 52; i++)
+for (i = 0 < 52; i++;)
 {
+putchar(alp[i]);
+}
 putchar('\n');
 return (0);
 }
