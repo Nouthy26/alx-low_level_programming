821ca9e5667baca7c4dec24ff553955b3ae45261
Nouthy26 <nkosinothando67@gmail.com>
2023-07-14 13:39:05 -0700

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - A program that prints all alphabets except q and e.
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
