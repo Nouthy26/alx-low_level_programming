f1074bba846e630c244610c2b5da50a4f45af6de
Nouthy26 <nkosinothando67@gmail.com>
2023-07-14 13:56:52 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Aprogram that prins numbers.
+* Return: 0 (Suceess)
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
