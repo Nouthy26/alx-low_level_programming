2b4cf753f9932ccbb34e8a67bb9ee57af209bf7f
Nouthy26 <nkosinothando67@gmail.com>
2023-07-14 11:38:54 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - A program that prints the alphabet in lowercase
+* Return:  0 (Success)
+*/
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
